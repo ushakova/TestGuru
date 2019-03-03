fa5ccd3c3c5d2d2e65d627c3a5bc85c15ae6a5da
Yunna Ushakova <79104745091@yandex.ru>
2019-03-03 19:48:30 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = User.create([{ name: 'Bob' }, { name: 'Bill' }, { name: 'Mary' }])
+categories = Category.create([{ title: 'Math' },
+                              { title: 'Physics' },
+                              { title: 'History' }])
+tests = Test.create([{ title: 'Optics', level: 3, category_id: 2 },
+                     { title: 'Relativity theory', level: 3, category_id: 2 },
+                     { title: 'Arithmetic operations', level: 0, category_id: 1 }])
+question = Question.create(body: 'mass-energy equivalence formula', test_id: 2)
+answers = Answer.create([{ body: 'E = m * c^2', correct: true, question_id: 1 },
+                         { body: '2pi * r', correct: false, question_id: 1 }])
