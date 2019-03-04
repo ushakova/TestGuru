9d99bd29a644e9ce54a72ea83e39cf89ac0f99fa
Yunna Ushakova <79104745091@yandex.ru>
2019-03-04 23:17:05 +0300

    add author

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_03_164922) do
+ActiveRecord::Schema.define(version: 2019_03_04_194318) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2019_03_03_164922) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
