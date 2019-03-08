3c1c22cc282d2c25c69b1a6fabb509fe69af5943
Yunna Ushakova <79104745091@yandex.ru>
2019-03-08 22:22:41 +0300

    fix association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :results
   has_many :tests, through: :results
-  has_many :tests, foreign_key: :author_id
+  has_many :own_tests, class_name: 'Test', foreign_key: :author_id
 
   def tests_by_level(level)
     tests.where(level: level)
