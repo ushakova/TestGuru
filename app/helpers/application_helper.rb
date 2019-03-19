45a9acf87584cfa0c8e9abb49395f27f5f3cde75
Yunna Ushakova <79104745091@yandex.ru>
2019-03-19 23:15:58 +0300

    add shared

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def current_year
+    Time.current.year
+  end
+
+  def github_url(author, repo)
+    link_to 'Github', "https://github.com/#{author}/#{repo}"
+  end
 end
